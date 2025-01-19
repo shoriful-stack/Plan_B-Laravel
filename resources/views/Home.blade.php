a709136ac8606989ce667ad155d0c7f303a6f7cf
shoriful-stack <sisazid182713@gmail.com>
2025-01-19 17:23:37 +0600

    bootstrap connected

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,1 +1,14 @@
-<h1> {{$result}} </h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel App</title>
+    <link rel="stylesheet" href="{{asset("css/bootstrap.css")}}">
+</head>
+<body>
+<button class="btn-success">Import</button>
+
+<script src="{{asset("js/bootstrap.bundle.js")}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/{num1}/{num2}", [HomeController::class, "page"]);
+Route::get("/", [HomeController::class, "page"]);
